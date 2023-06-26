27582bfb56600f0d1050466ec047eefd82158573
juliezhangahs <137836411+juliezhangahs@users.noreply.github.com>
2023-06-26 16:00:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,3 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner Breakfast Menu Omega 3 & Blueberry Oatmeal Kale, Spinach & Rocket Smoothie Har-Boiled Free-Range Egg')
+streamlit.title('My Parents New Healthy Diner Breakfast Menu 
+Omega 3 & Blueberry Oatmeal Kale, Spinach & Rocket Smoothie Har-Boiled Free-Range Egg')
