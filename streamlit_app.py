3c2665e1828fdcb27fb88ee17c461521a39ad864
juliezhangahs <137836411+juliezhangahs@users.noreply.github.com>
2023-06-30 17:10:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
   else:
     back_from_function=get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-
+streamlit.error()
 
 streamlit.header("The fruit load list contains:")
 
